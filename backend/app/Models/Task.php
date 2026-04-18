e21a6fbb91c624d55b5e0356a8a657ac126aac46
wilson.anjos <wilsonanjos_@live.com>
2026-04-18 19:53:54 -0300

    feat: implementar soft deletes para Task e Comment

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
