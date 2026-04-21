874376d32cd45a789f9921ccbbe60e47957a06a1
wilson.anjos <wilsonanjos_@live.com>
2026-04-21 18:27:12 -0300

    chore: add railway deployment configurations and cors origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000/', 'http://localhost:8000', 'http://127.0.0.1:8000'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/^https?:\/\/.*\.railway\.app$/'],
 
     'allowed_headers' => ['*'],
 
